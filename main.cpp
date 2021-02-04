55568e4787e9938edeeecfdb57cf6d202dd55c91
qmclouca <qmclouca@gmail.com>
2021-02-04 11:07:16 -0300

    URI Online Judge | 1175 Array change I Adapted by Neilor Tonin, URI  Brazil
    Timelimit: 1
    Write a program that reads an array N [20]. After, change the first element by the last, the second element by the last but one, etc.., Up to change the 10th to the 11th. print the modified array.
    
    Input
    The input contains 20 integer numbers, positive or negative.
    
    Output
    For each position of the array N print "N[i] = Y", where i is the array position and Y is the number stored in that position.
    
    Input Sample	Output Sample
    0
    -5
    ...
    63
    230
    
    N[0] = 230
    N[1] = 63
    ...
    N[18] = -5
    N[19] = 0
    
    Thanks to Cassio F.
    
    URI Accepted!

--- main.cpp
+++ main.cpp
@@ -1,24 +1,13 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    int N[20], buffer = 0, index=0;
+    int N[20];
     for (int aux = 0; aux<20; aux++){
         N[aux] = 100000;
     }
     for (int aux = 0; aux<20; aux++){
         std::cin >> N[aux];
-    }
-    for (int aux = 0; aux<20; aux++) {
-        buffer = N[aux];
-        //std::cout<<"buffer: "<<buffer<<std::endl;
-        index = 19-aux;
-        std::cout<<"index: "<<index<<std::endl;
-        //N[aux] = N[index];
-        std::cout<<"N[aux]: "<<N[aux]<<std::endl;
-        //N[index] = buffer;
-        std::cout<<"N[index]: "<<N[index]<<std::endl;
     }
     for (int aux = 0; aux<20; aux++) {
         std::cout << "N[" <<aux<<"] = "<< N[19-aux]<<std::endl;
